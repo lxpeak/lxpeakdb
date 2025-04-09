a5c0a366c28bc86f993799c1c994aa535cee6b80
lxpeak <742907278@qq.com>
2025-04-09 16:47:18 +0800

    修改单元测试代码

--- src/test/java/com/lxpeak/mydb/backend/server/ExecutorTest.java
+++ src/test/java/com/lxpeak/mydb/backend/server/ExecutorTest.java
@@ -11,13 +11,14 @@ import org.junit.Test;
 import com.lxpeak.mydb.backend.tbm.TableManager;
 
 public class ExecutorTest {
-    String path = "D://mydb/dbTest/mydb";
+    String path1 = "D://mydb/dbTest/ExecutorTestDB";
+    String path2 = "D://mydb/dbTest/ExecutorTestDB2";
     long mem = (1 << 20) * 64;
 
     byte[] CREATE_TABLE = "create table test_table id int32 (index id)".getBytes();
     byte[] INSERT = "insert into test_table values 2333".getBytes();
 
-    private Executor testCreate() throws Exception {
+    private Executor testCreate(String path) throws Exception {
         TransactionManager tm = TransactionManager.create(path);
         DataManager dm = DataManager.create(path, mem, tm);
         VersionManager vm = VersionManager.newVersionManager(tm, dm);
@@ -36,16 +37,16 @@ public class ExecutorTest {
 
     @Test
     public void testInsert10000() throws Exception {
-        Executor exe = testCreate();
+        Executor exe = testCreate(path1);
         testInsert(exe, 10000, 1);
-        new File(path + ".db").delete();
-        new File(path + ".bt").delete();
-        new File(path + ".log").delete();
-        new File(path + ".xid").delete();
+        new File(path1 + ".db").delete();
+        new File(path1 + ".bt").delete();
+        new File(path1 + ".log").delete();
+        new File(path1 + ".xid").delete();
     }
 
     private void testMultiInsert(int total, int noWorkers) throws Exception {
-        Executor exe = testCreate();
+        Executor exe = testCreate(path2);
         // 这里必须用不同的executor，否则会出现并发问题
         TableManager tbm = exe.tbm;
         int w = total/noWorkers;
@@ -70,9 +71,9 @@ public class ExecutorTest {
     @Test
     public void test100000With4() throws Exception {
         testMultiInsert(10000, 4);
-        new File(path + ".db").delete();
-        new File(path + ".bt").delete();
-        new File(path + ".log").delete();
-        new File(path + ".xid").delete();
+        new File(path2 + ".db").delete();
+        new File(path2 + ".bt").delete();
+        new File(path2 + ".log").delete();
+        new File(path2 + ".xid").delete();
     }
 }
