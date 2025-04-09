9f40c2ca75185c83bec3f75ebeff172fb554ca05
lxpeak <742907278@qq.com>
2025-04-09 16:20:38 +0800

    修改事务锁的删除逻辑

--- src/main/java/com/lxpeak/mydb/backend/vm/LockTable.java
+++ src/main/java/com/lxpeak/mydb/backend/vm/LockTable.java
@@ -89,6 +89,12 @@ public class LockTable {
                     selectNewXID(uid);
                 }
             }
+            // debug
+            // 对waitU包含的(xid, uid)进行了删除，但是好像wait的(uid, xid)记录没有删除，会导致wait.get(uid)的数组永远不为空，被删除的xid仍在等待获取uid
+            Long waitUid = waitU.get(xid);
+            if (waitUid != null)
+                removeFromList(wait, waitUid, xid);
+
             waitU.remove(xid);
             x2u.remove(xid);
             waitLock.remove(xid);
@@ -111,6 +117,7 @@ public class LockTable {
                 continue;
             } else {
                 u2x.put(uid, xid);
+                putIntoList(x2u, xid, uid);
                 Semaphore lo = waitLock.remove(xid);
                 waitU.remove(xid);
                 lo.release();
